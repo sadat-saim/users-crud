7f1320f3734985e637269ae4513d4b6f9da22adb
Sadat Saim <saimk861@gmail.com>
2022-12-08 19:51:31 +0600

    ui updated

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -7,7 +7,7 @@ const Card = ({ user }) => {
 
   return (
     <div className="relative">
-      <div className="flex flex-col justify-center max-w-xs p-6 shadow-md rounded-xl sm:px-12 bg-gray-50 text-gray-800">
+      <div className="flex flex-col justify-center p-6 shadow-md rounded-xl bg-gray-50 text-gray-800 w-full h-full">
         <img
           src={photoURL}
           alt=""
@@ -22,22 +22,40 @@ const Card = ({ user }) => {
           </div>
         </div>
         {loggedInUser && (
-          <button className="btn btn-sm btn-circle btn-outline absolute top-3 right-6">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M6 18L18 6M6 6l12 12"
-              />
-            </svg>
-          </button>
+          <>
+            <button className="btn btn-sm btn-circle btn-outline absolute top-3 left-16">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+            <button className="btn btn-sm btn-circle btn-outline absolute top-3 left-6">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+                />
+              </svg>
+            </button>
+          </>
         )}
       </div>
     </div>
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,21 @@
 import React, { useState } from "react";
-import { collection, getDoc, getDocs } from "firebase/firestore";
+import { useForm } from "react-hook-form";
+import { addDoc, collection, getDoc, getDocs } from "firebase/firestore";
 import { db } from "../../firebase/firebase.init";
 import { useQuery } from "@tanstack/react-query";
 import Card from "../../components/Card/Card";
 import Loading from "../../components/Loading/Loading";
+import { async } from "@firebase/util";
+import toast from "react-hot-toast";
 
 const Home = () => {
+  const { register, handleSubmit } = useForm();
   const usersCollection = collection(db, "users");
-  const { data: users, isLoading } = useQuery({
+  const {
+    data: users,
+    isLoading,
+    refetch,
+  } = useQuery({
     queryKey: ["users"],
     queryFn: async () => {
       const data = await getDocs(usersCollection);
@@ -17,15 +25,76 @@ const Home = () => {
   });
   console.log(users);
 
+  const handleAddUser = async (data) => {
+    console.log(data);
+    const { name, email, image } = data;
+    try {
+      await addDoc(usersCollection, {
+        displayName: name,
+        email,
+        photoURL: image,
+        emailVerified: false,
+      });
+      toast.success("Added user successfully");
+      refetch();
+    } catch (err) {
+      toast.error(`${err.message}`);
+    }
+  };
+
   if (isLoading) return <Loading></Loading>;
 
   return (
     <div>
-      <h1 className="text-center font-bold text-3xl mt-3 mb-6">All Users</h1>
+      <div className="flex justify-between my-3">
+        <h1 className="font-bold text-3xl mt-3 mb-6">All Users</h1>
+        <label htmlFor="my-modal-6" className="btn">
+          Add User
+        </label>
+      </div>
       <div className="grid grid-cols-3 gap-3">
         {users?.map((user) => (
           <Card key={user.id} user={user}></Card>
         ))}
+      </div>
+
+      <input type="checkbox" id="my-modal-6" className="modal-toggle" />
+      <div className="modal modal-bottom sm:modal-middle">
+        <div className="modal-box">
+          <h3 className="font-bold text-xl text-center">Add A User</h3>
+          <div className="py-4">
+            <form
+              className="text-center"
+              onSubmit={handleSubmit(handleAddUser)}
+            >
+              <input
+                type="text"
+                placeholder="name"
+                className="input input-bordered w-full max-w-xs"
+                {...register("name", { required: true })}
+              />
+              <input
+                type="text"
+                placeholder="email"
+                className="input input-bordered w-full max-w-xs my-3"
+                {...register("email", { required: true })}
+              />
+              <input
+                type="text"
+                placeholder="photo url..."
+                className="input input-bordered w-full max-w-xs"
+                {...register("image", { required: true })}
+              />
+              <div className="modal-action">
+                <button className="mx-auto">
+                  <label htmlFor="my-modal-6" className="btn text-center">
+                    Add User
+                  </label>
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
     </div>
   );
